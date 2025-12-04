49e49bb515ad689990571c1544d306724fb02e96
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-12-04 15:12:00 +0100

    Implement Convex authentication configuration and client provider with Clerk integration.

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -2,6 +2,8 @@
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { useAuth } from '@clerk/nextjs'
+import { ConvexProviderWithClerk } from "convex/react-clerk";
 
 if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
     throw new Error("NEXT_PUBLIC_CONVEX_URL is not set");
@@ -10,5 +12,5 @@ if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
-    return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+    return <ConvexProviderWithClerk useAuth={useAuth} client={convex}>{children}</ConvexProviderWithClerk>;
 }
