9ac1a0afb6706c695ef0cdef897d82f6d732c9c1
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-12-15 13:10:00 +0100

    correctly apply authentication protection to specified routes in proxy middleware

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 const protectedRoutes = createRouteMatcher(["/dashboard(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (!protectedRoutes(req)) {
+    if (protectedRoutes(req)) {
         await auth.protect()
     }
 })
